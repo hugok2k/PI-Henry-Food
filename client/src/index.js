c3c8af4cf0cbd8585af418e5b508d68d05ed1b67
Hugo Avila <hugok2k@gmail.com>
2022-08-08 23:01:57 -0300

    fix deploy

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API;
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.render(
   <React.StrictMode>
